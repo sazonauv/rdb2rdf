9e0142edeee9fdb24d4b2b93db8fbf3ef24b6844
slava-sazonau <sazonauv@gmail.com>
2017-02-27 18:07:57 +0000

    Initial commit

---
+++ src/main/java/uk/ac/man/cs/rdb2rdf/main/TestConnection.java
@@ -0,0 +1,32 @@
+package uk.ac.man.cs.rdb2rdf.main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+/**
+ * Created by slava on 27/02/17.
+ */
+public class TestConnection {
+    public static void main(String args[]) {
+        Connection c = null;
+        try {
+            String url = "jdbc:postgresql://therealjasmina.cs.man.ac.uk:5432/HealtheFacts";
+            Properties props = new Properties();
+            props.setProperty("user", "slava");
+            props.setProperty("password", "");
+            props.setProperty("sslmode", "verify-full");
+            props.setProperty("sslrootcert", "/home/slava/.postgresql/root.crt");
+            props.setProperty("sslcert", "/home/slava/.postgresql/slava.crt");
+            props.setProperty("sslkey", "/home/slava/.postgresql/slava.key");
+
+            // get a connection
+            c = DriverManager.getConnection(url, props);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
+}
