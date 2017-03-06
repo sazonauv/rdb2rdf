a00a57b09be83c2f272c6140940146125eb25971
slava-sazonau <sazonauv@gmail.com>
2017-03-06 10:37:21 +0000

    u

--- src/main/java/uk/ac/man/cs/rdb2rdf/main/CSV2OWLConverter.java
+++ src/main/java/uk/ac/man/cs/rdb2rdf/main/CSV2OWLConverter.java
@@ -3,7 +3,9 @@ package uk.ac.man.cs.rdb2rdf.main;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.*;
 
+import javax.xml.namespace.QName;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -16,80 +18,87 @@ public class CSV2OWLConverter {
     private static final String IRI_NAME = "http://owl.cs.manchester.ac.uk/healthefacts";
     private static final String IRI_DELIMITER = "#";
     private static final String ENTITY_DELIMITER = "-";
+    private static final String CSV_DELIMITER = ",";
 
 
-    private List<String[]> rows;
-    private String[] header;
 
-    private OWLOntologyManager manager;
-    private OWLOntology ontology;
-    private OWLDataFactory factory;
+    private void createOntology(File csvFile, File ontFile)
+            throws OWLOntologyCreationException,
+            IOException, OWLOntologyStorageException {
 
-
-    public CSV2OWLConverter(File csvFile) {
-        init(csvFile);
-    }
-
-    private void init(File csvFile) {
-        // read the CSV file
-        rows = CSVReader.read(csvFile);
-        header = rows.get(0);
-        rows.remove(0);
-    }
-
-
-    private void createOntology() throws OWLOntologyCreationException {
         // create an ontology
         IRI iri = IRI.create(IRI_NAME);
-        manager = OWLManager.createOWLOntologyManager();
-        ontology = manager.createOntology(iri);
-        factory = manager.getOWLDataFactory();
+        OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+        OWLOntology ontology = manager.createOntology(iri);
+        OWLDataFactory factory = manager.getOWLDataFactory();
 
         // populate the ontology
-        int i = 0;
-        for (String[] row : rows) {
-            i++;
-            // encounter
-            String encId = row[0];
-            encId = encId.replaceAll("\"", "");
-            IRI encIRI = IRI.create(IRI_NAME + IRI_DELIMITER + encId + ENTITY_DELIMITER + i);
-            OWLNamedIndividual encInd = factory.getOWLNamedIndividual(encIRI);
-            // gender
-            String gender = row[2];
-            gender = gender.replaceAll("\"", "");
-            IRI genderIRI = IRI.create(IRI_NAME + IRI_DELIMITER + gender);
-            OWLClass genderClass = factory.getOWLClass(genderIRI);
-            // measurements
-            String meas = row[3].replaceAll(" ", "");
-            meas = meas.replaceAll("\"", "");
-            IRI measIRI = IRI.create(IRI_NAME + IRI_DELIMITER + meas);
-            OWLObjectProperty measProp = factory.getOWLObjectProperty(measIRI);
-            // results of measurements
-            String measRes = row[4].replaceAll(" ", "");
-            measRes = measRes.replaceAll("\"", "");
-            IRI measResIRI = IRI.create(IRI_NAME + IRI_DELIMITER + measRes);
-            OWLClass measResClass = factory.getOWLClass(measResIRI);
-            IRI measResIndIRI = IRI.create(IRI_NAME + IRI_DELIMITER + measRes + ENTITY_DELIMITER + "i");
-            OWLNamedIndividual measResInd = factory.getOWLNamedIndividual(measResIndIRI);
-            // medical conditions
-            String cond = row[5].replaceAll(" ", "");
-            cond = cond.replaceAll("\"", "");
-            IRI condIRI = IRI.create(IRI_NAME + IRI_DELIMITER + cond);
-            OWLClass condClass = factory.getOWLClass(condIRI);
-            // axioms
-            Set<OWLAxiom> axioms = new HashSet<>();
-            axioms.add(factory.getOWLClassAssertionAxiom(genderClass, encInd));
-            axioms.add(factory.getOWLClassAssertionAxiom(condClass, encInd));
-            axioms.add(factory.getOWLClassAssertionAxiom(measResClass, measResInd));
-            axioms.add(factory.getOWLObjectPropertyAssertionAxiom(measProp, encInd, measResInd));
-            manager.addAxioms(ontology, axioms);
+        BufferedReader reader = null;
+        try {
+            if (csvFile.exists()) {
+                reader = new BufferedReader(new FileReader(csvFile));
+                // read the header
+                String line = reader.readLine();
+                // read the file line by line
+                int i = 0;
+                while ((line = reader.readLine()) != null) {
+                    // get all tokens in the line
+                    String[] row = line.split(CSV_DELIMITER);
+                    // debug
+                    i++;
+                    if (i % 1e6 == 0) {
+                        System.out.println(i + " lines are processed");
+                    }
+                    // encounter
+                    String encId = processCell(row[0]);
+                    IRI encIRI = IRI.create(IRI_NAME + IRI_DELIMITER + encId);
+                    OWLNamedIndividual encInd = factory.getOWLNamedIndividual(encIRI);
+                    // measurements
+                    String meas = processCell(row[1]);
+                    IRI measIRI = IRI.create(IRI_NAME + IRI_DELIMITER + meas);
+                    if (measIRI.toString().contains("http://owl.cs.manchester.ac.uk/healthefacts#Temperature")) {
+                        continue;
+                    }
+                    OWLObjectProperty measProp = factory.getOWLObjectProperty(measIRI);
+                    // results of measurements
+                    String measRes = processCell(row[2]);
+                    IRI measResIRI = IRI.create(IRI_NAME + IRI_DELIMITER + measRes);
+                    OWLClass measResClass = factory.getOWLClass(measResIRI);
+                    IRI measResIndIRI = IRI.create(IRI_NAME + IRI_DELIMITER + measRes + ENTITY_DELIMITER + "i");
+                    OWLNamedIndividual measResInd = factory.getOWLNamedIndividual(measResIndIRI);
+                    // medical conditions
+                    String cond = processCell(row[3]);
+                    IRI condIRI = IRI.create(IRI_NAME + IRI_DELIMITER + cond);
+                    OWLClass condClass = factory.getOWLClass(condIRI);
+                    // patient types
+                    String ptype = processCell(row[4]);
+                    IRI ptypeIRI = IRI.create(IRI_NAME + IRI_DELIMITER + ptype);
+                    OWLClass ptypeClass = factory.getOWLClass(ptypeIRI);
+                    // medications
+                    String med = processCell(row[5]);
+                    IRI medIRI = IRI.create(IRI_NAME + IRI_DELIMITER + med);
+                    OWLClass medClass = factory.getOWLClass(medIRI);
+                    // axioms
+                    Set<OWLAxiom> axioms = new HashSet<>();
+                    axioms.add(factory.getOWLClassAssertionAxiom(condClass, encInd));
+                    axioms.add(factory.getOWLClassAssertionAxiom(measResClass, measResInd));
+                    axioms.add(factory.getOWLObjectPropertyAssertionAxiom(measProp, encInd, measResInd));
+                    axioms.add(factory.getOWLClassAssertionAxiom(ptypeClass, encInd));
+                    axioms.add(factory.getOWLClassAssertionAxiom(medClass, encInd));
+                    manager.addAxioms(ontology, axioms);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
-    }
-
-
-
-    private void saveOntology(File ontFile) throws IOException, OWLOntologyStorageException {
+        // save the ontology
         if (!ontFile.exists()) {
             ontFile.createNewFile();
         }
@@ -98,16 +107,21 @@ public class CSV2OWLConverter {
         manager.saveOntology(ontology, buffOutputStream);
     }
 
+    private static String processCell(String str) {
+        return str.replace(" ", "").replace("\"", "")
+                .replace("\'", "").replace("\n", "")
+                .replace("\t", "").replace("\r", "")
+                .replace("\\", ENTITY_DELIMITER)
+                .replace("/", ENTITY_DELIMITER);
+    }
+
+
 
     public static void main(String args[])
             throws OWLOntologyCreationException,
             IOException, OWLOntologyStorageException {
-        File csvFile = new File(args[0]);
-        CSV2OWLConverter converter = new CSV2OWLConverter(csvFile);
-        converter.createOntology();
-        // save the ontology
-        File ontFile = new File(args[1]);
-        converter.saveOntology(ontFile);
+        CSV2OWLConverter converter = new CSV2OWLConverter();
+        converter.createOntology(new File(args[0]), new File(args[1]));
     }
 
 
